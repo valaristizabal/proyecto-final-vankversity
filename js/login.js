91ef8263e23c2c8bcfacb30fb9e6b326240a4f57
Valeria <05valarist@gmail.com>
2025-02-24 14:44:31 -0500

    avance interfaz de crear cueta

--- js/login.js
+++ js/login.js
@@ -1,35 +1,12 @@
 function login() {
-    let user = document.getElementById("user").value;
-    let password = document.getElementById("password").value;
+    let correo = document.getElementById("correo").value;
+    let contrasena = document.getElementById("contrasena").value;
 
-    if (user === "admin" && password === "123") {
+    if (correo === "admin" && contrasena === "123") {
         window.location.href = "inicio.html";
     } else {
         alert("Usuario o contraseña incorrectos");
     }
 }
-
-// Modo nocturno
-const toggleModeBtn = document.getElementById("toggleMode");
-const body = document.body;
-
-// Cargar modo nocturno si estaba activado
-if (localStorage.getItem("darkMode") === "enabled") {
-    body.classList.add("dark-mode");
-    toggleModeBtn.textContent = "☀️ Modo Claro";
-}
-
-toggleModeBtn.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-
-    if (body.classList.contains("dark-mode")) {
-        localStorage.setItem("darkMode", "enabled");
-        toggleModeBtn.textContent = "☀️ Modo Claro";
-    } else {
-        localStorage.setItem("darkMode", "disabled");
-        toggleModeBtn.textContent = "🌙 Modo Nocturno";
-    }
-});
-
 
 
